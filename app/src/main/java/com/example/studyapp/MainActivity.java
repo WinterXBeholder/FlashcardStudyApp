9095e33d60f76c514b660de3dd8e49cfdd3d34ba
Brian <brian.hacker@ire.minnstate.edu>
2020-07-21 19:05:14 -0500

    Started creating an activity which will create the flachard content. + edit text to activity_main.xml + submit button to activity_main.xml + click event and string variables to MainActivity.java

--- app/src/main/java/com/example/studyapp/MainActivity.java
+++ app/src/main/java/com/example/studyapp/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.studyapp;
 
 import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -10,12 +11,29 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button SubmitPrinciple = (Button) findViewById(R.id.SubmitPrinciple)
+        SubmitPrinciple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText PrincipleNameEntry = (EditText) findViewById(R.id.PrincipleNameEntry);
+                EditText PrincipleDescriptionEntry = (EditText) findViewById(R.id.PrincipleDescriptionEntry);
+                EditText PrincipleFormulaEntry = (EditText) findViewById(R.id.PrincipleFormulaEntry);
+                String PrincipleName = PrincipleNameEntry.getText().toString();
+                String PrincipleDescription = PrincipleDescriptionEntry.getText().toString();
+                String PrincipleFormula = PrincipleFormulaEntry.getText().toString();
+
+            }
+        });
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
